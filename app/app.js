743dcb1d8f9c8717981f3998821cdf235dbac160
Tom <tomyitav@gmail.com>
2017-04-15 17:18:36 +0300

    Moving to ui-router - first step

--- app/app.js
+++ app/app.js
@@ -1,25 +1,23 @@
 import angular from 'angular'
 import ngRoute from 'angular-route'
-
+import angularUIRouter from 'angular-ui-router';
 import cars from 'cars/cars'
 import apolloServ from 'apollo/apollo-wrapper-service-module'
 import view2 from 'view2/view2'
 import version from 'components/version/version'
-import ApolloWrapperService from 'apollo/apollo-wrapper-service'
 
 var myApp = angular
   .module('myApp', [
     ngRoute,
+      angularUIRouter,
     version.name,
     cars.name,
       apolloServ.name,
     view2.name,
   ])
-  .config(($locationProvider, $routeProvider) => {
+  .config(($locationProvider, $stateProvider, $urlRouterProvider) => {
     $locationProvider.hashPrefix('!')
-    $routeProvider.otherwise({
-      redirectTo: '/cars'
-    })
+      $urlRouterProvider.otherwise('/cars');
   })
 
 angular
